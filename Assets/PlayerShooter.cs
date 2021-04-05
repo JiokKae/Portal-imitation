42c4785b9a5fd318e15925dec1ebe1fe01a9eb48
JiokKae <ekaktm1@gmail.com>
2021-04-06 03:44:37 +0900

    Create PlayerShooter.cs

---
+++ Assets/PlayerShooter.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooter : MonoBehaviour
+{
+    public Transform gunPivot;
+
+    private Animator playerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnAnimatorIK(int layerIndex)
+	{
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, gunPivot.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.RightHand, gunPivot.rotation);
+    }
+}
